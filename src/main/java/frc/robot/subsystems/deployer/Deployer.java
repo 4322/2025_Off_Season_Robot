d89da392d319a363df8de275258c095f9e7c2c36
Ryan Yeung <ryanyeung@frc4322.com>
2025-08-06 08:27:11 -0700

    Added IntakeSuperstructure, Deployer, EndEffector, Indexer, and Rollers subsystems classes and basic layouts

---
+++ src/main/java/frc/robot/subsystems/deployer/Deployer.java
@@ -0,0 +1,37 @@
+package frc.robot.subsystems.deployer;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Deployer extends SubsystemBase {
+    
+    public Deployer() {
+
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+
+    public void deploy() {
+
+    }
+
+    public void retract() {
+
+    }
+
+    public void ejectPosition() {
+        
+    }
+    
+    public void setHome() {
+
+    }
+
+    public void setNeutralMode(NeutralMode mode) {
+
+    }
+
+    
+}
---
+++ src/main/java/frc/robot/subsystems/endEffector/EndEffector.java
@@ -0,0 +1,53 @@
+package frc.robot.subsystems.endEffector;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class EndEffector extends SubsystemBase {
+
+    public enum EndEffectorStates {
+
+    }
+
+    public EndEffector(){
+
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+
+    public void idle() {
+
+    }
+
+    public void intakeAlgae() {
+
+    }
+
+    public void intakeCoral() {
+
+    }
+
+    public void relaeseCoral() {
+
+    }
+
+    public void releaseAlgae() {
+
+    }
+
+    public boolean hasAlgae() {
+        return true; // temp
+    }
+
+    public boolean hasCoral() {
+        return true; // temp
+    }
+
+    public void setNeutralMode(NeutralMode mode) {
+        
+    }
+
+
+}
