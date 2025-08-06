e49876670591b2db43513497164db8a7f2104c25
Ellie Morse <elliemorse@frc4322.com>
2025-08-06 15:49:19 -0700

    deleted uneeded code

--- src/main/java/frc/robot/subsystems/arm/ArmIO.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIO.java
@@ -5,12 +5,10 @@ import org.littletonrobotics.junction.AutoLog;
 public interface ArmIO {
   @AutoLog
   public static class ArmIOInputs {
-    public double positionDeg = 0.0;
+    public double positionDeg = 0.0; // Horizontal to front of robot is 0 degrees
     public double velocityDegPerSec = 0.0;
     public double appliedVolts = 0.0;
   }
 
   public default void updateInputs(ArmIOInputs inputs) {}
-
-  public default void setVoltage(double volts) {}
 }
