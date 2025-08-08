7a9786530349447e8fe13d88fc5a60b93a162a38
Ellie Morse <elliemorse@frc4322.com>
2025-08-07 18:45:25 -0700

    Got rid of booleans and started adding to constants

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -9,107 +9,41 @@ public class Arm extends SubsystemBase {
     this.io = ArmIO;
   }
 
-  private boolean armIdle;
-  private boolean algaeHold;
-  private boolean coralHold;
-  private boolean algaeGround;
-  private boolean algaeReef;
-  private boolean scoreAlgae;
-  private boolean prescoreCoral;
-  private boolean scoreCoral;
-  private boolean safeBargeRetract;
-  private boolean getAngleDegrees;
-  private boolean setHome;
-  private boolean setNeutralMode;
-  private boolean climbing;
-  private boolean eject;
-
-  public void armIdle() {
-    unsetAllRequests();
-    armIdle = true;
+  public void idle() {
+    // Set the arm to idle state
+    // This could mean stopping the motors or setting a default position
+    
   }
 
-  public void algaeHold() {
-    unsetAllRequests();
-    algaeHold = true;
-  }
+  public void algaeHold() {}
 
-  public void coralHold() {
-    unsetAllRequests();
-    coralHold = true;
-  }
+  public void coralHold() {}
 
-  public void algaeGround() {
-    unsetAllRequests();
-    algaeGround = true;
-  }
+  public void algaeGround() {}
 
-  public void algaeReef(/*requestedLevel*/ ) {
-    unsetAllRequests();
-    algaeReef = true;
-  }
+  public void algaeReef(/*requestedLevel*/ ) {}
 
-  public void scoreAlgae(/*ArmState scoringSide*/ ) {
-    unsetAllRequests();
-    scoreAlgae = true;
-  }
+  public void scoreAlgae(/*ArmState scoringSide*/ ) {}
 
-  public void prescoreCoral(/*ArmState requestedLevel*/ ) {
-    unsetAllRequests();
-    prescoreCoral = true;
-  }
+  public void prescoreCoral(/*ArmState requestedLevel*/ ) {}
 
-  public void scoreCoral(/*ArmState requestedLevel*/ ) {
-    unsetAllRequests();
-    scoreCoral = true;
-  }
+  public void scoreCoral(/*ArmState requestedLevel*/ ) {}
 
   public boolean atSetpoint() {
     // TODO: Implement logic to check if arm is at setpoint
     return true;
   }
 
-  public void safeBargeRetract() {
-    unsetAllRequests();
-    safeBargeRetract = true;
-  }
+  public void safeBargeRetract() {}
 
-  public void setHome() {
-    unsetAllRequests();
-    setHome = true;
-  }
+  public void setHome() {}
 
-  public void setNeutralMode(/*ArmState mode*/ ) {
-    unsetAllRequests();
-    setNeutralMode = true;
-  }
+  public void setNeutralMode(/*ArmState mode*/ ) {}
 
-  public void climbing() {
-    unsetAllRequests();
-    climbing = true;
-  }
+  public void climbing() {}
 
-  public void eject() {
-    unsetAllRequests();
-    eject = true;
-  }
+  public void eject() {}
 
   public void getAngleDegrees() {}
 
-  private void unsetAllRequests() {
-    armIdle = false;
-    algaeHold = false;
-    coralHold = false;
-    algaeGround = false;
-    algaeReef = false;
-    scoreAlgae = false;
-    prescoreCoral = false;
-    scoreCoral = false;
-    safeBargeRetract = false;
-    getAngleDegrees = false;
-    setHome = false;
-    setNeutralMode = false;
-    climbing = false;
-    eject = false;
-  }
 }
--- src/main/java/frc/robot/subsystems/arm/ArmIOReal.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIOReal.java
@@ -1,3 +1,11 @@
 package frc.robot.subsystems.arm;
 
-public class ArmIOReal implements ArmIO {}
+public class ArmIOReal implements ArmIO {
+    private Nitrate ArmMotor;
+
+    private NitrateSettings ArmMotorConfig = new NitrateSettings();
+
+    public ArmIOReal() {
+
+    }
+}
