4ea5f5e56d0f761a9e1a580b194fd3a62bbb66c2
Ellie Morse <elliemorse@frc4322.com>
2025-08-09 10:23:40 -0700

    Added exsamples and angles

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -1,17 +1,59 @@
 package frc.robot.subsystems.arm;
 
+import com.reduxrobotics.motorcontrol.nitrate.Nitrate;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase {
   private ArmIO io;
 
+  private static final double SAFE_ARM_ANGLE = 45.0;
+  private static final double MIN_SAFE_ARM_ANGLE = 20.0;
+  private static final double MAX_SAFE_ARM_ANGLE = 20.0;
+
+  /* double currentPosition = arm.getPositionDeg();
+  double requestedPosition = requestedPositionDeg.get();
+
+  // First check if arm can move optimized
+  if (currentPosition < 0) {
+    if (requestedPosition > 0) {
+      if (Math.abs(requestedPosition - currentPosition) > 180) {
+        requestedPosition -= 360;
+      }
+    }
+  } else if (currentPosition > 0) {
+    if (requestedPosition < 0) {
+      if (Math.abs(requestedPosition - currentPosition) > 180) {
+        requestedPosition += 360;
+      }
+    }
+  }
+
+  if (requestedPosition > 270) {
+    requestedPosition -= 360;
+  } else if (requestedPosition < -270) {
+    requestedPosition += 360;
+  } */
   public Arm(ArmIO ArmIO) {
     this.io = ArmIO;
   }
 
+  public Arm() {
+    // TODO Auto-generated constructor stub
+  }
+
+  public enum Safety {
+    SAFE,
+    LOWERING,
+    WAIT_FOR_ELEVATOR,
+    MOVING_WITH_ELEVATOR,
+    IDLE
+  }
+
+  Safety safety = Safety.SAFE;
+
   public void idle() {
-    // Set the arm to idle state
-    // This could mean stopping the motors or setting a default position
+    Nitrate.class.cast(io).setVoltage(0.0);
 
   }
 
