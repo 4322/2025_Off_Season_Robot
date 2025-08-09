039f3da46d1711d6408a59e5c03f8e132386b8bf
Dhrish Karangula <dhrishkarangula@frc4322.com>
2025-08-09 11:51:48 -0700

    fix turn pid initializaition bug in drivetrain constants and add todo

--- src/main/java/frc/robot/constants/DrivetrainConstants.java
+++ src/main/java/frc/robot/constants/DrivetrainConstants.java
@@ -8,12 +8,12 @@ import frc.robot.util.SwerveUtil.SwerveModuleConstantsFactory;
 
 public class DrivetrainConstants {
   // Shared drivetrain constants
-  public static final int gyroID = 0;
-  public static final double maxSpeedAt12Volts = 4.0;
+  public static final int gyroID = 0; // TODO
+  public static final double maxSpeedAt12Volts = 4.0; // TODO
 
-  private static final double driveGearRatio = 7.363636363636365;
-  private static final double turnGearRatio = 15.42857142857143;
-  private static final double turnCoupleRatio = 3.8181818181818183;
+  private static final double driveGearRatio = 7.363636363636365; // TODO
+  private static final double turnGearRatio = 150.0 / 7.0;
+  private static final double turnCoupleRatio = 3.8181818181818183; // TODO
 
   private static final double wheelRadius = Units.inchesToMeters(2.0);
 
@@ -22,59 +22,59 @@ public class DrivetrainConstants {
   private static final boolean invertLeftSideDrive = false;
   private static final boolean invertRightSideDrive = true;
 
-  public static final double driveSupplyCurrentLimit = 40.0;
-  public static final double driveSupplyCurrentTime = 0.5;
-  public static final double driveStatorCurrentLimit = 100;
+  public static final double driveSupplyCurrentLimit = 40.0; // TODO
+  public static final double driveSupplyCurrentTime = 0.5; // TODO
+  public static final double driveStatorCurrentLimit = 100; // TODO
 
-  public static final double turnSupplyCurrentLimit = 40.0;
-  public static final double turnSupplyCurrentTime = 0.5;
-  public static final double turnStatorCurrentLimit = 100;
-  private static final double drivekP = 0;
-  private static final double drivekD = 0;
-  private static final double drivekS = 0;
-  private static final double drivekV = 0;
+  public static final double turnSupplyCurrentLimit = 40.0; // TODO
+  public static final double turnSupplyCurrentTime = 0.5; // TODO
+  public static final double turnStatorCurrentLimit = 100; // TODO
+  private static final double drivekP = 0; // TODO
+  private static final double drivekD = 0; // TODO
+  private static final double drivekS = 0; // TODO
+  private static final double drivekV = 0; // TODO
 
-  private static final double turnkP = 0;
-  private static final double turnkD = 0;
-  private static final double turnAccelerationLimit = 0;
-  private static final double turnDeaccelerationLimit = 0;
-  private static final double turnVelocityLimit = 0;
+  private static final double turnkP = 0; // TODO
+  private static final double turnkD = 0; // TODO
+  private static final double turnAccelerationLimit = 0; // TODO
+  private static final double turnDeaccelerationLimit = 0; // TODO
+  private static final double turnVelocityLimit = 0; // TODO
 
-  private static final double simTurnInertia = 0.00001;
-  private static final double simDriveInertia = 0.001;
+  private static final double simTurnInertia = 0.00001; // TODO
+  private static final double simDriveInertia = 0.001; // TODO
 
-  // Specific module constants
+  // Specific module constants:
   // Front Left
-  private static final int frontLeftDriveMotorId = 0;
-  private static final int frontLeftTurnMotorId = 0;
-  private static final int frontLeftTurnEncoderId = 0;
+  private static final int frontLeftDriveMotorId = 0; // TODO
+  private static final int frontLeftTurnMotorId = 0; // TODO
+  private static final int frontLeftTurnEncoderId = 0; // TODO
 
-  private static final double frontLeftXPos = 0;
-  private static final double frontLeftYPos = 0;
+  private static final double frontLeftXPos = 0; // TODO
+  private static final double frontLeftYPos = 0; // TODO
 
   // Front Right
-  private static final int frontRightDriveMotorId = 0;
-  private static final int frontRightTurnMotorId = 0;
-  private static final int frontRightTurnEncoderId = 0;
+  private static final int frontRightDriveMotorId = 0; // TODO 
+  private static final int frontRightTurnMotorId = 0; // TODO
+  private static final int frontRightTurnEncoderId = 0; // TODO
 
-  private static final double frontRightXPos = 0;
-  private static final double frontRightYPos = 0;
+  private static final double frontRightXPos = 0; // TODO
+  private static final double frontRightYPos = 0; // TODO
 
   // Back Left
-  private static final int backLeftDriveMotorId = 0;
-  private static final int backLeftTurnMotorId = 0;
-  private static final int backLeftTurnEncoderId = 0;
+  private static final int backLeftDriveMotorId = 0; // TODO
+  private static final int backLeftTurnMotorId = 0; // TODO
+  private static final int backLeftTurnEncoderId = 0; // TODO
 
-  private static final double backLeftXPos = 0;
-  private static final double backLeftYPos = 0;
+  private static final double backLeftXPos = 0; // TODO
+  private static final double backLeftYPos = 0; // TODO
 
   // Back Right
-  private static final int backRightDriveMotorId = 0;
-  private static final int backRightTurnMotorId = 0;
-  private static final int backRightTurnEncoderId = 0;
+  private static final int backRightDriveMotorId = 0; // TODO
+  private static final int backRightTurnMotorId = 0; // TODO
+  private static final int backRightTurnEncoderId = 0; // TODO
 
-  private static final double backRightXPos = 0;
-  private static final double backRightYPos = 0;
+  private static final double backRightXPos = 0; // TODO
+  private static final double backRightYPos = 0; // TODO
 
   // Auto-configured objects below
   public static final ElectricalLimitSettings driveElectricalLimitSettings =
@@ -103,7 +103,7 @@ public class DrivetrainConstants {
   private static final SwerveModuleConstantsFactory moduleCreator =
       new SwerveModuleConstantsFactory()
           .withDriveMotorGains(drivePIDSettings)
-          .withTurnMotorGains(drivePIDSettings)
+          .withTurnMotorGains(turnPIDSettings)
           .withDriveElectricalLimitSettings(driveElectricalLimitSettings)
           .withTurnElectricalLimitSettings(turnElectricalLimitSettings)
           .withDriveMotorGearRatio(driveGearRatio)
