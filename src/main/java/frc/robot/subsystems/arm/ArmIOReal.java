79642c9baa6600903a98eaece8448b1ee2475b95
Ellie Morse <elliemorse@frc4322.com>
2025-08-09 11:03:05 -0700

    Revert "Changed file name"
    This reverts commit 82cea9803e749761ed65333e7dca517163f2a51f.

--- src/main/java/frc/robot/subsystems/arm/ArmIOReal.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIOReal.java
@@ -3,10 +3,10 @@ package frc.robot.subsystems.arm;
 import com.reduxrobotics.motorcontrol.nitrate.Nitrate;
 import com.reduxrobotics.motorcontrol.nitrate.NitrateSettings;
 
-public class ArmIONitrate implements ArmIO {
+public class ArmIOReal implements ArmIO {
   private Nitrate ArmMotor;
 
   private NitrateSettings ArmMotorConfig = new NitrateSettings();
 
-  public ArmIONitrate() {}
+  public ArmIOReal() {}
 }
