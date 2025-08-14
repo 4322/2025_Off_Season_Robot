9b68d9b2c7006137e48529c005a9d0db4ba4e554
Ryan Yeung <ryanyeung@frc4322.com>
2025-08-14 13:08:33 -0700

    Update DeltaDebouncer to have a cumulative mode

--- src/main/java/frc/robot/util/DeltaDebouncer.java
+++ src/main/java/frc/robot/util/DeltaDebouncer.java
@@ -4,48 +4,86 @@ package frc.robot.util;
 import edu.wpi.first.wpilibj.Timer;
 
 public class DeltaDebouncer {
+    public enum Mode {
+        INSTANT, // Only trigger if a single change is greater than the threshold
+        CUMULATIVE // Trigger if smaller changes add up to be greater than the threshold
+    }
+
     private final double deltaThreshold;
     private final double debounceTimeSeconds;
+    private final double maxAccumulationSeconds;
+    private final Mode mode;
 
     private double lastValue;
+    private double baselineValue;
     private boolean firstUpdate = true;
 
-    private Timer timer = new Timer();
+    private Timer debounceTimer = new Timer();
+    private Timer accumulationTimer = new Timer();
 
-    DeltaDebouncer(double debounceTimeSeconds, double deltaThreshold) {
+    DeltaDebouncer(double debounceTimeSeconds, double deltaThreshold, Mode mode, double maxAccumulationSeconds) {
         this.debounceTimeSeconds = debounceTimeSeconds;
         this.deltaThreshold = deltaThreshold;
-        timer.reset();
-        timer.start();
+        if (mode == Mode.CUMULATIVE) {
+            this.maxAccumulationSeconds = maxAccumulationSeconds;
+        } else {
+            this.maxAccumulationSeconds = 0;
+        }
+        this.mode = mode;
+        debounceTimer.reset();
+        debounceTimer.start();
+        accumulationTimer.reset();
+        accumulationTimer.start();
     }
 
     public boolean calculate(double value) {
         // Don't trigger on first update
         if (firstUpdate) {
             lastValue = value;
+            baselineValue = value;
             firstUpdate = false;
             return false;
         }
 
-        double delta = Math.abs(value - lastValue);
+        double delta;
+        if (mode == Mode.INSTANT) {
+            delta = Math.abs(value - lastValue);
+        } else {
+            delta = Math.abs(value - baselineValue);
+        }
 
         // Checking if delta has exceeded the threshold for long enough
         if (delta >= deltaThreshold) {
-            if (!timer.isRunning()) {
-                timer.reset();
-                timer.start();
+            if (!debounceTimer.isRunning()) {
+                debounceTimer.reset();
+                debounceTimer.start();
             }
 
-            if (timer.hasElapsed(debounceTimeSeconds)) {
+            if (debounceTimer.hasElapsed(debounceTimeSeconds)) {
                 lastValue = value;
-                timer.stop();
+                baselineValue = value;
+                debounceTimer.stop();
+                accumulationTimer.reset();
+                accumulationTimer.start();
+                firstUpdate = true;
                 return true;
             }
         } else {
-            timer.stop(); // Reset if the delta is below the threshold
-            timer.reset();
+            if (mode == Mode.INSTANT) {
+                debounceTimer.stop(); // Reset if the delta is below the threshold
+                debounceTimer.reset();
+            } else {
+                // Reset accumulation if maxAccumulationSeconds have passed
+                if (accumulationTimer.hasElapsed(maxAccumulationSeconds)) {
+                    baselineValue = value;
+                    accumulationTimer.reset();
+                    accumulationTimer.start();
+                }
+            }
+            
         }
 
+        lastValue = value;
         return false;
     }
 }
