8bef5afc507f8082d7cab08afd133cbc4f4880fc
Ryan Yeung <ryanyeung@frc4322.com>
2025-08-18 16:51:19 -0700

    Updated PID request in Deployer

--- src/main/java/frc/robot/subsystems/deployer/DeployerIONitrate.java
+++ src/main/java/frc/robot/subsystems/deployer/DeployerIONitrate.java
@@ -9,6 +9,7 @@ import com.reduxrobotics.motorcontrol.nitrate.types.IdleMode;
 import com.reduxrobotics.motorcontrol.nitrate.types.MotionProfileMode;
 import com.reduxrobotics.motorcontrol.nitrate.types.MotorType;
 import com.reduxrobotics.motorcontrol.nitrate.types.PIDConfigSlot;
+import com.reduxrobotics.motorcontrol.requests.PIDPositionRequest;
 import com.reduxrobotics.sensors.canandmag.Canandmag;
 import com.reduxrobotics.sensors.canandmag.CanandmagSettings;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -20,6 +21,11 @@ public class DeployerIONitrate implements DeployerIO {
 
   private NitrateSettings deployerMotorConfig = new NitrateSettings();
   private CanandmagSettings deployerMotorEncoderConfig = new CanandmagSettings();
+
+  private final PIDPositionRequest deployerMotorDeployPIDRequest =
+      new PIDPositionRequest(PIDConfigSlot.kSlot0, 0).useMotionProfile(true);
+  private final PIDPositionRequest deployerMotorRetractPIDRequest =
+      new PIDPositionRequest(PIDConfigSlot.kSlot1, 0).useMotionProfile(true);
 
   
   private double previousRequestedPosition = -999;
@@ -128,17 +134,15 @@ public class DeployerIONitrate implements DeployerIO {
   }
 
   @Override
-  public void setDeployerMotorPosition(double rotations) { // TODO update this to work like Ellie's code
-    // TODO figure out if deploy is greater than or less than current rotations
+  public void setDeployerMotorPosition(double rotations) {
+    // TODO This is assuming deploying is positive direction
     if (rotations != previousRequestedPosition) {
-      deployerMotorRequestedPositionRotations = rotations;
+      deployerMotorRequestedPositionRotations = rotations * Constants.Deployer.motorGearRatio;
       previousRequestedPosition = rotations;
-      if (deployerMotor.getPosition() > rotations) {
-        deployerMotor.setPIDPosition(
-            PIDConfigSlot.kSlot0, rotations * Constants.Deployer.motorGearRatio, 0.0, true);
+      if (deployerMotor.getPosition() < rotations * Constants.Deployer.motorGearRatio) {
+        deployerMotor.setRequest(deployerMotorDeployPIDRequest.setPosition(rotations * Constants.Deployer.motorGearRatio));
       } else {
-        deployerMotor.setPIDPosition(
-            PIDConfigSlot.kSlot1, rotations * Constants.Deployer.motorGearRatio, 0.0, true);
+        deployerMotor.setRequest(deployerMotorRetractPIDRequest.setPosition(rotations * Constants.Deployer.motorGearRatio));
       }
     }
   }
