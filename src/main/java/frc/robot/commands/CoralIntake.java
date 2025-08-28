d5a262fccab4de8a79d009af83ba18f5abe88328
Ryan Yeung <ryanyeung@frc4322.com>
2025-08-28 15:43:04 -0700

    Add CoralIntake command

--- src/main/java/frc/robot/commands/CoralIntake.java
+++ src/main/java/frc/robot/commands/CoralIntake.java
@@ -1,21 +1,47 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.IntakeSuperstructure;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.vision.objectDetection.VisionObjectDetection;
+
 
 public class CoralIntake extends Command {
-  public CoralIntake() {}
 
-  @Override
-  public void initialize() {}
+  private IntakeSuperstructure intakeSuperstructure;
+  private Drive drive;
+  private VisionObjectDetection visionObjectDetection;
+  private Translation2d coralPosition;
 
-  @Override
-  public void execute() {}
-
-  @Override
-  public boolean isFinished() {
-    return false; // TODO have actual logic
+  public CoralIntake(IntakeSuperstructure intakeSuperstructure, Drive drive, VisionObjectDetection visionObjectDetection) {
+    this.intakeSuperstructure = intakeSuperstructure;
+    this.drive = drive;
+    this.visionObjectDetection = visionObjectDetection;
   }
 
   @Override
-  public void end(boolean interrupted) {}
+  public void initialize() {
+
+  }
+
+  @Override
+  public void execute() {
+    coralPosition = visionObjectDetection.getClosestCoralPositionRelativeToCamera();
+    intakeSuperstructure.requestIntake();
+    if (coralPosition != null) {
+      // TODO vibrate controller
+      // TODO Math/methods for turning/driving towards coral
+    }
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    intakeSuperstructure.requestRetractIdle();
+  }
 }
