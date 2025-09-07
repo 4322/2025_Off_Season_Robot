93358480a233309d425653e4d5d1c6bc33fef51e
Doug Kavner <dougkavner@frc4322.com>
2025-09-07 15:35:24 -0700

    Fix merge error

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Robot;
 import frc.robot.subsystems.Superstructure.Level;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.drive.Drive;
