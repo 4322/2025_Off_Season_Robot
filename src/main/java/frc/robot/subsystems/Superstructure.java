c40c2c3d71470820caa2f94d4e91d8d5e51d04c4
Ellie Morse <elliemorse@frc4322.com>
2025-09-11 15:56:48 -0700

    Change boolean logic in is autooperation

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,7 +12,6 @@ import frc.robot.subsystems.elevator.Elevator;
 import frc.robot.subsystems.endEffector.EndEffector;
 import frc.robot.subsystems.indexer.Indexer;
 import frc.robot.subsystems.vision.Vision;
-import org.littletonrobotics.junction.Logger;
 
 public class Superstructure extends SubsystemBase {
   public static final Timer startTimer = new Timer();
@@ -305,11 +306,7 @@ public class Superstructure extends SubsystemBase {
   }
 
   public boolean isAutoOperationMode() {
-    if (mode == OperationMode.AUTO) {
-      return true;
-    } else {
-      return false;
-    }
+    return mode == OperationMode.AUTO;
   }
 
   public void requestIdle() {
