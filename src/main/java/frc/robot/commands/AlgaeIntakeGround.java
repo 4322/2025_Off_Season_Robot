34a0fdd55ed6b4c3a17dc496a4c95138d77dd196
Ryan Yeung <ryanyeung@frc4322.com>
2025-09-11 15:28:14 -0700

    Add AlgaeIntakeGround command

---
+++ src/main/java/frc/robot/commands/AlgaeIntakeGround.java
@@ -0,0 +1,35 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.IntakeSuperstructure;
+import frc.robot.subsystems.Superstructure;
+import static frc.robot.RobotContainer.driver;
+
+public class AlgaeIntakeGround extends Command {
+  private Superstructure superstructure;
+
+  public AlgaeIntakeGround(Superstructure superstructure) {
+    this.superstructure = superstructure;
+
+    addRequirements(superstructure);
+  }
+
+  @Override
+  public void initialize() {}
+
+  @Override
+  public void execute() {
+    superstructure.requestIntakeAlgaeFloor();
+
+    if (!driver.a().getAsBoolean() || superstructure.isAlgaeHeld()) {
+       cancel();
+    }
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    if (!superstructure.isAlgaeHeld()) {
+        superstructure.requestIdle();
+    } // Superstructure automatically handles the transition back to algae idle
+  }
+}
