b000c48d5504516971468b04d2d62bcc7e537ada
Ellie Morse <elliemorse@frc4322.com>
2025-09-15 17:28:07 -0700

    Add the buffer

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,7 +12,6 @@ import frc.robot.subsystems.elevator.Elevator;
 import frc.robot.subsystems.endEffector.EndEffector;
 import frc.robot.subsystems.indexer.Indexer;
 import frc.robot.subsystems.vision.Vision;
-import org.littletonrobotics.junction.Logger;
 
 public class Superstructure extends SubsystemBase {
   public static final Timer startTimer = new Timer();
@@ -159,8 +160,10 @@ public class Superstructure extends SubsystemBase {
 
         break;
       case ALGAE_PRESCORE:
-        arm.scoreAlgae();
         elevator.scoreAlgae();
+        if (elevator.atSetpoint()) {
+          arm.scoreAlgae();
+        }
 
         if (!requestAlgaePrescore) {
           state = Superstates.ALGAE_IDLE;
