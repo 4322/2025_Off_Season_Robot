503b4002a172eadd530b6983b605b92af1beb604
Ryan Yeung <ryanyeung@frc4322.com>
2025-09-16 16:14:50 -0700

    Update a few thigs

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -151,7 +151,7 @@ public class RobotContainer {
         intakeSuperstructure = new IntakeSuperstructure(endEffector, deployer, rollers, indexer);
         superstructure =
             new Superstructure(
-                endEffector, arm, indexer, elevator, drive, vision, intakeSuperstructure);
+                endEffector, arm, elevator, drive, intakeSuperstructure);
         break;
 
       case SIM:
