e06ff87862d0bdfcf9ea6a5a01b6d33992b8130a
Dhrish Karangula <dhrishkarangula@frc4322.com>
2025-09-24 22:29:42 -0700

    add getYawVelocity method for drive to use in drive to pose command

--- src/main/java/frc/robot/subsystems/drive/GyroIOBoron.java
+++ src/main/java/frc/robot/subsystems/drive/GyroIOBoron.java
@@ -1,9 +1,11 @@
 package frc.robot.subsystems.drive;
 
 import com.reduxrobotics.sensors.canandgyro.Canandgyro;
+import com.reduxrobotics.sensors.canandgyro.CanandgyroSettings;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
 import frc.robot.constants.DrivetrainConstants;
 
 public class GyroIOBoron implements GyroIO {
@@ -11,6 +13,14 @@ public class GyroIOBoron implements GyroIO {
 
   public GyroIOBoron() {
     gyro = new Canandgyro(DrivetrainConstants.gyroId);
+
+    CanandgyroSettings settings = new CanandgyroSettings();
+    settings.setAngularVelocityFramePeriod(0.02);
+    CanandgyroSettings gyroConfigStatus = gyro.setSettings(settings, 0.02, 5);
+
+    if (!gyroConfigStatus.isEmpty()) {
+      DriverStation.reportError("Gyro failed to configure", false);
+    }
   }
 
   @Override
