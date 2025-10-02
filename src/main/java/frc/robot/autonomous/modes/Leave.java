272848e6f2af0d21678963893f992e4602a6765d
Ryan Yeung <ryanyeung@frc4322.com>
2025-10-01 22:27:04 -0700

    Template for leave auto

---
+++ src/main/java/frc/robot/autonomous/modes/Leave.java
@@ -0,0 +1,18 @@
+package frc.robot.autonomous.modes;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Robot;
+import frc.robot.subsystems.drive.Drive;
+
+public class Leave extends SequentialCommandGroup{
+    public Leave(Drive drive) {
+        setName("LEAVE");
+        addRequirements(drive);
+        /*addCommands(
+            new AutoPoseReset(swerve, Robot.Leave.getStartingHolonomicPose().get().getTranslation()),
+        AutoBuilder.followPath(Robot.Leave)
+        );*/
+    }
+}
