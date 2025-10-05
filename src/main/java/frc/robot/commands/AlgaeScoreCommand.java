e813b0b18e2e7047e8ac11fc6dd1d58988b8cb4b
Ellie Morse <elliemorse@frc4322.com>
2025-10-04 18:29:46 -0700

    Comment out auto rotate

--- src/main/java/frc/robot/commands/AlgaeScoreCommand.java
+++ src/main/java/frc/robot/commands/AlgaeScoreCommand.java
@@ -1,12 +1,8 @@
 package frc.robot.commands;
 
-import static frc.robot.RobotContainer.driver;
-
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Robot;
 import frc.robot.RobotContainer;
+import static frc.robot.RobotContainer.driver;
 import frc.robot.subsystems.Superstructure;
 import frc.robot.subsystems.drive.Drive;
 
@@ -22,11 +18,11 @@ public class AlgaeScoreCommand extends Command {
 
   @Override
   public void initialize() {
-    if (Robot.alliance == DriverStation.Alliance.Blue) {
-      drive.requestAutoRotateMode(Rotation2d.fromDegrees(0));
-    } else {
-      drive.requestAutoRotateMode(Rotation2d.fromDegrees(180));
-    }
+    // if (Robot.alliance == DriverStation.Alliance.Blue) {
+    //   drive.requestAutoRotateMode(Rotation2d.fromDegrees(0));
+    // } else {
+    //   drive.requestAutoRotateMode(Rotation2d.fromDegrees(180));
+    // }
     superstructure.requestAlgaePrescore();
   }
 
