6acaaaee2cd589ce83064d46ae2d74647f5953f1
Ellie Morse <elliemorse@frc4322.com>
2025-10-05 18:05:28 -0700

    Changed so always in manual mode

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -61,7 +61,7 @@ public class Superstructure extends SubsystemBase {
     MANUAL,
   }
 
-  OperationMode mode = OperationMode.AUTO;
+  OperationMode mode = OperationMode.MANUAL;
 
   Superstates state = Superstates.UNHOMED;
 
