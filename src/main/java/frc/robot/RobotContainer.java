0b3d701f70204093b2d932309c920f182789983a
Dhrish Karangula <dhrishkarangula@frc4322.com>
2025-10-07 16:09:19 -0700

    slightly modify drive to pose tuning to be used on red or blue side

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -299,17 +299,22 @@ public class RobotContainer {
                     () ->
                         intakeSuperstructure.getIntakeSuperstate()
                             != IntakeSuperstructure.IntakeSuperstates.HOMELESS));
-    // Drive to Reef Poses for Testing
-      Rotation2d robotReefAngle = reefStatus.getClosestRobotAngle();
-      driver.povRight().onTrue(new InstantCommand(() -> {
-        new DriveToPose(drive, () -> new Pose2d(
-          FieldConstants.KeypointPoses.leftReefBranchScoringRed.rotateAround(
-              FieldConstants.KeypointPoses.redReefCenter,
-              robotReefAngle.rotateBy(Rotation2d.k180deg)),
-          robotReefAngle)).schedule();
-      })
-      .onlyIf(() -> Constants.enableDriveToPoseTuning)
-      );
+      
+      if (Constants.enableDriveToPoseTuning) {
+        driver.povRight().onTrue(new InstantCommand(() -> {
+          Translation2d scoringPos;
+          Rotation2d rotation;
+          if (Robot.alliance == Alliance.Blue) {
+            scoringPos = FieldConstants.KeypointPoses.rightReefBranchScoringBlue;
+            scoringPos = scoringPos.rotateAround(FieldConstants.KeypointPoses.blueReefCenter, Rotation2d.k180deg);
+            rotation = Rotation2d.kZero;
+          } else {
+            scoringPos = FieldConstants.KeypointPoses.rightReefBranchScoringRed;
+            rotation = Rotation2d.k180deg;
+          }
+          new DriveToPose(drive, new Pose2d(scoringPos, rotation)).schedule();
+        }));
+      }
   }
 
   public static boolean isScoringTriggerHeld() {
