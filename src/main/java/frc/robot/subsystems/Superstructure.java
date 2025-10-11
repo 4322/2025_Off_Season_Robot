63cce3391678e5106bd751c82a612b62f018e8be
Ellie Morse <elliemorse@frc4322.com>
2025-10-11 08:19:35 -0700

    CHnage it so starts in auto in superstructure

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems;
 
+import org.littletonrobotics.junction.Logger;
+
 import com.reduxrobotics.motorcontrol.nitrate.types.IdleMode;
+
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -12,7 +15,6 @@ import frc.robot.subsystems.endEffector.EndEffector.EndEffectorStates;
 import frc.robot.subsystems.vision.Vision;
 import frc.robot.subsystems.vision.VisionIO.SingleTagCamera;
 import frc.robot.util.ReefStatus;
-import org.littletonrobotics.junction.Logger;
 
 public class Superstructure extends SubsystemBase {
   public static final Timer startTimer = new Timer();
@@ -64,7 +66,7 @@ public class Superstructure extends SubsystemBase {
     MANUAL,
   }
 
-  OperationMode mode = OperationMode.MANUAL;
+  OperationMode mode = OperationMode.AUTO;
 
   Superstates state = Superstates.HOMELESS;
 
