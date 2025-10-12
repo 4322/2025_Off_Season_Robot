53859b9c47f79ce91ec5e1fec64420196fe5e5bc
Ryan Yeung <ryanyeung@frc4322.com>
2025-10-11 18:04:12 -0700

    Changes

--- src/main/java/frc/robot/autonomous/modes/TestAuto.java
+++ src/main/java/frc/robot/autonomous/modes/TestAuto.java
@@ -4,11 +4,15 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.Robot;
 import frc.robot.commands.ScoreCoral;
+import frc.robot.commands.auto.CoralIntakeManualAuto;
 import frc.robot.subsystems.IntakeSuperstructure;
 import frc.robot.subsystems.Superstructure;
 import frc.robot.subsystems.Superstructure.Level;
+import frc.robot.subsystems.Superstructure.Superstates;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.vision.Vision;
 
@@ -18,10 +22,17 @@ public class TestAuto extends Command {
   private IntakeSuperstructure intakeSuperstructure;
   private Vision vision;
 
-  private ScoreCoral scoreCoral;
+  private ScoreCoral scoreCoral1;
+    private ScoreCoral scoreCoral2;
+    private CoralIntakeManualAuto coralIntakeManualAuto;
   private Command ThreeCoralStartToJuliet;
+  private Command JulietToFeed;
+    private Command FeedToKilo;
 
-  private boolean started;
+
+  private int currentCommand = 0;
+
+  private Command[] commands = new Command[5];
 
   public TestAuto(
       Superstructure superstructure,
@@ -33,14 +44,27 @@ public class TestAuto extends Command {
     this.intakeSuperstructure = intakeSuperstructure;
     this.vision = vision;
 
-    this.started = false;
-
-    this.scoreCoral = new ScoreCoral(superstructure, Level.L4, drive);
+    this.scoreCoral1 = new ScoreCoral(superstructure, Level.L4, drive);
+    this.scoreCoral2 = new ScoreCoral(superstructure, Level.L4, drive);
     this.ThreeCoralStartToJuliet = AutoBuilder.followPath(Robot.ThreeCoralStartToJuliet);
-  }
+    this.coralIntakeManualAuto = new CoralIntakeManualAuto(intakeSuperstructure, true);
+    this.JulietToFeed = AutoBuilder.followPath(Robot.JulietToFeed);
+
+    this.commands[0] = ThreeCoralStartToJuliet;
+    this.commands[1] = scoreCoral1;
+    this.commands[2] = new ParallelCommandGroup(
+        coralIntakeManualAuto,
+        JulietToFeed
+    );
+
+    }
+    
+  
 
   @Override
   public void initialize() {
+    
+
     superstructure.requestOperationMode(Superstructure.OperationMode.AUTO);
     PathPlannerPath path = Robot.ThreeCoralStartToJuliet;
     if (Robot.alliance == Alliance.Red) {
@@ -52,16 +76,11 @@ public class TestAuto extends Command {
 
   @Override
   public void execute() {
-    if (ThreeCoralStartToJuliet.isFinished()) {
-      if (!scoreCoral.isScheduled()) {
-        scoreCoral.schedule();
-        started = true;
-      }
-    }
+    
   }
 
   @Override
   public boolean isFinished() {
-    return scoreCoral.isFinished() && started;
+    return false;
   }
 }
