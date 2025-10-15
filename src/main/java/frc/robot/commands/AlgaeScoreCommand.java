424c9fb56dcb755f8cd0cfb9806dc3fcbfcb3419
Doug Kavner <dougkavner@frc4322.com>
2025-10-14 18:20:33 -0700

    Formatting

--- src/main/java/frc/robot/commands/AlgaeScoreCommand.java
+++ src/main/java/frc/robot/commands/AlgaeScoreCommand.java
@@ -1,11 +1,12 @@
 package frc.robot.commands;
 
+import static frc.robot.RobotContainer.driver;
+
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Robot;
 import frc.robot.RobotContainer;
-import static frc.robot.RobotContainer.driver;
 import frc.robot.subsystems.Superstructure;
 import frc.robot.subsystems.drive.Drive;
 
@@ -22,11 +23,11 @@ public class AlgaeScoreCommand extends Command {
   @Override
   public void initialize() {
     superstructure.requestAlgaePrescore();
-      if (Robot.alliance == DriverStation.Alliance.Red) {
-        drive.requestAutoRotateMode(Rotation2d.fromDegrees(0));
-      } else {
-        drive.requestAutoRotateMode(Rotation2d.fromDegrees(180));
-      }
+    if (Robot.alliance == DriverStation.Alliance.Red) {
+      drive.requestAutoRotateMode(Rotation2d.fromDegrees(0));
+    } else {
+      drive.requestAutoRotateMode(Rotation2d.fromDegrees(180));
+    }
   }
 
   @Override
