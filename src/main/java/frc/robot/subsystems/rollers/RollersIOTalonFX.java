a1ad2f808c3b5d174ca6934d5c0e889c0e78a890
Ryan Yeung <ryanyeung@frc4322.com>
2025-10-21 21:40:07 -0700

    Update error message for RollersIOTalonFX

--- src/main/java/frc/robot/subsystems/rollers/RollersIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/rollers/RollersIOTalonFX.java
@@ -36,7 +36,7 @@ public class RollersIOTalonFX implements RollersIO {
       DriverStation.reportError(
           "Talon "
               + rollersMotor.getDeviceID()
-              + " error (End Effector): "
+              + " error (Rollers): "
               + feederConfigStatus.getDescription(),
           false);
     }
