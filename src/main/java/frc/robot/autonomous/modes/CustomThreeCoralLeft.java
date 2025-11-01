9d3eeadf68432d0286ae2a11e926a3ea214a8540
Ellie Morse <elliemorse@frc4322.com>
2025-10-31 23:51:56 -0700

    Made it degrees instead

--- src/main/java/frc/robot/autonomous/modes/CustomThreeCoralLeft.java
+++ src/main/java/frc/robot/autonomous/modes/CustomThreeCoralLeft.java
@@ -2,9 +2,9 @@ package frc.robot.autonomous.modes;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.PathPlannerPath;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
@@ -39,45 +39,51 @@ public class CustomThreeCoralLeft extends OrangeSequentialCommandGroup {
         new ReefStatus(
             false,
             false,
-            new Rotation2d(
-                Units.degreesToRadians(
-                    Robot.alliance == Alliance.Blue
-                        ? ReefFaceRobotHeading.IJ.blue
-                        : ReefFaceRobotHeading.IJ.red)),
+            () -> {
+              return Robot.alliance == Alliance.Blue
+                  ? Rotation2d.fromDegrees(ReefFaceRobotHeading.IJ.blue)
+                  : Rotation2d.fromDegrees(ReefFaceRobotHeading.IJ.red);
+            },
             ClosestReefPipe.RIGHT,
             L1Zone.MIDDLE,
             AlgaeLevel.L2,
-            Robot.alliance == Alliance.Blue ? ReefFaceTag.IJ.idBlue : ReefFaceTag.IJ.idRed);
+            () -> {
+              return Robot.alliance == Alliance.Blue ? ReefFaceTag.IJ.idBlue : ReefFaceTag.IJ.idRed;
+            });
 
     ReefStatus reefCoral2 =
         new ReefStatus(
             false,
             false,
-            new Rotation2d(
-                Units.degreesToRadians(
-                    Robot.alliance == Alliance.Blue
-                        ? ReefFaceRobotHeading.KL.blue
-                        : ReefFaceRobotHeading.KL.red)),
+            () -> {
+              return Robot.alliance == Alliance.Blue
+                  ? Rotation2d.fromDegrees(ReefFaceRobotHeading.KL.blue)
+                  : Rotation2d.fromDegrees(ReefFaceRobotHeading.KL.red);
+            },
             ClosestReefPipe.LEFT,
             L1Zone.MIDDLE,
             AlgaeLevel.L2,
-            Robot.alliance == Alliance.Blue ? ReefFaceTag.KL.idBlue : ReefFaceTag.KL.idRed);
+            () -> {
+              return Robot.alliance == Alliance.Blue ? ReefFaceTag.KL.idBlue : ReefFaceTag.KL.idRed;
+            });
 
     ReefStatus reefCoral3 =
         new ReefStatus(
             false,
             false,
-            new Rotation2d(
-                Units.degreesToRadians(
-                    Robot.alliance == Alliance.Blue
-                        ? ReefFaceRobotHeading.KL.blue
-                        : ReefFaceRobotHeading.KL.red)),
+            () -> {
+              return Robot.alliance == Alliance.Blue
+                  ? Rotation2d.fromDegrees(ReefFaceRobotHeading.KL.blue)
+                  : Rotation2d.fromDegrees(ReefFaceRobotHeading.KL.red);
+            },
             ClosestReefPipe.RIGHT,
             L1Zone.MIDDLE,
             AlgaeLevel.L2,
-            Robot.alliance == Alliance.Blue ? ReefFaceTag.KL.idBlue : ReefFaceTag.KL.idRed);
+            () -> {
+              return Robot.alliance == Alliance.Blue ? ReefFaceTag.KL.idBlue : ReefFaceTag.KL.idRed;
+            });
 
-    PathPlannerPath path = Robot.CustomThreeCoralStartToJuliet;
+    PathPlannerPath path = Robot.ThreeCoralStartToJuliet;
     Pose2d startPoseBlue = path.getStartingHolonomicPose().get();
     path = path.flipPath();
     Pose2d startPoseRed = path.getStartingHolonomicPose().get();
