bbbf7a5ee136d1ced1d9a071fdc28ff3d7c37104
Ellie Morse <elliemorse@frc4322.com>
2025-10-31 23:55:20 -0700

    Formatting

--- src/main/java/frc/robot/autonomous/modes/CustomThreeCoralLeft.java
+++ src/main/java/frc/robot/autonomous/modes/CustomThreeCoralLeft.java
@@ -2,7 +2,6 @@ package frc.robot.autonomous.modes;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.PathPlannerPath;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
