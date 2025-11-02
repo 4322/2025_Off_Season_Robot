016306eb700c63be9f5ce47dd652162b1d1607ba
Ellie Morse <elliemorse@frc4322.com>
2025-11-02 11:40:02 -0800

    Safety logic so that we can be at min elevator safeheight

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -86,7 +86,7 @@ public class Arm extends SubsystemBase {
               // safe angle and the elevator is too low the arm
               // will go to min safe angle
               newSetpoint = minSafeArmDegree;
-            } else if (requestedSetpoint > Constants.Arm.armIdleDeg && elevatorHeight < (minElevatorHeight)){
+            } else if (requestedSetpoint > Constants.Arm.armIdleDeg && elevatorHeight < (minElevatorHeight - Constants.Elevator.bufferHeightMeters)){
               newSetpoint = Constants.Arm.armIdleDeg; 
             } //So we dont move arm when in cradle
             else {
